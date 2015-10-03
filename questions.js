bb133e56cca4da8812c0bc5dc19063232b373b54
mdubard <mary.dubard96@gmail.com>
2015-10-03 15:24:48 -0400

    more buttons

--- questions.js
+++ questions.js
@@ -1,7 +1,24 @@
+var questionTracker = 2;
+var questions = ["What is an array?", "2+2=?", "How do you dance?"];
+var answersA =  ["1",                 "1",     "1"];
+var answersB =  ["2",                 "2",     "2"];
+var answersC =  ["3",                 "3",     "3"];
+var answersD =  ["4",                 "4",     "4"];
+
 function wrongAnswer(){
 	alert("Wrong Answer");
 }
 
 function rightAnswer(){
 	alert("Right Answer");
+}
+
+function nextQuestion(){
+document.getElementById("questionHead").innerHTML = "Question " + questionTracker;
+document.getElementById("question").innerHTML = questions[questionTracker-2];
+document.getElementById("a").innerHTML = answersA[questionTracker-2];
+document.getElementById("b").innerHTML = answersB[questionTracker-2];
+document.getElementById("c").innerHTML = answersC[questionTracker-2];
+document.getElementById("d").innerHTML = answersD[questionTracker-2];
+questionTracker++;
 }
