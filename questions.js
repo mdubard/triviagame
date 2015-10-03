7ff6e3b1fef75ab1c20a3585fd37535723b3c56e
mdubard <mary.dubard96@gmail.com>
2015-10-03 15:47:04 -0400

    new

--- questions.js
+++ questions.js
@@ -1,16 +1,22 @@
 var questionTracker = 2;
+var score = 0;
 var questions = ["What is an array?", "2+2=?", "How do you dance?"];
 var answersA =  ["1",                 "1",     "1"];
 var answersB =  ["2",                 "2",     "2"];
 var answersC =  ["3",                 "3",     "3"];
 var answersD =  ["4",                 "4",     "4"];
+var points = [5, 5, 10, 10];
 
 function wrongAnswer(){
 	alert("Wrong Answer");
+	nextQuestion();
 }
 
 function rightAnswer(){
 	alert("Right Answer");
+	score += points[questionTracker-2];
+	document.getElementById("points").innerHTML = score;
+	nextQuestion();
 }
 
 function nextQuestion(){
